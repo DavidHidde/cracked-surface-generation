11e3e0d1da53f4a785a0d9f19d50c6b2cd706630
DavidHidde <dahibo@kpnmail.nl>
2023-09-21 19:34:51 +0200

    Add 2D crack generator as separate class, add many parameters and make pretty

--- crack-generation/crack_path_generation_playground.py
+++ crack-generation/crack_path_generation_playground.py
@@ -1,79 +1,93 @@
 import math
-import random
 
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button
-from scipy.stats import norm
-from math import cos, sin
+from crack_parameters import CrackParameters
+from crack_path_generator import CrackPathGenerator
 
-# Get the rotation matrix for a certain angle
-def get_rotation_matrix(angle):
-    return np.array([
-        [cos(angle), -sin(angle)],
-        [sin(angle), cos(angle)]
-    ])
 
-# Create the Brownian motion crack
-def get_crack(variance, initial_angle, permutation_chance, length):
-    positions = np.zeros((length + 1, 2))
-    
-    # Initial angle
-    angle = initial_angle
-    
-    sigma_square = variance ** 2
-    for idx in range(length):
-        increments = norm.rvs(size=2, scale=sigma_square)
-        positions[idx + 1, :] = positions[idx, :] + np.dot(get_rotation_matrix(angle), np.array([1., increments[0]]))
-        angle += (1. if random.random() < permutation_chance else 0.) * increments[1] * math.pi
-        
-    return positions[:, 0], positions[:, 1]
+# Set the bounds of a plot
+def set_ax_bounds(ax, x, y):
+    min_x = math.floor(np.min(x) - 5)
+    max_x = math.ceil(np.max(x) + 5)
+    ax.set_xticks(np.linspace(min_x, max_x, 7, dtype=int))
+    ax.set_xlim(min_x, max_x)
+
+    min_y = math.floor(np.min(y) - 5)
+    max_y = math.ceil(np.max(y) + 5)
+    ax.set_yticks(np.linspace(min_y, max_y, 7, dtype=int))
+    ax.set_ylim(min_y, max_y)
+
 
 # Update plot
 def update_plot(value):
-    x, y = get_crack(variance_slider.val, angle_slider.val, chance_slider.val, LENGTH)
+    parameters = CrackParameters(
+        0,
+        INITIAL_WIDTH,
+        int(length_slider.val),
+        angle_slider.val,
+        variance_slider.val,
+        width_variation_slider.val,
+        int(start_slider.val),
+        int(end_slider.val),
+        chance_slider.val
+    )
+    x, y = crack_generator(parameters)
     line.set_xdata(x)
     line.set_ydata(y)
-    
-    # Set bounds again
-    min_y = np.min(y)
-    max_y = np.max(y)
-    offset = (max_y - min_y) / 6.
-    ax.set_yticks(np.linspace(min_y, max_y, 6))
-    ax.set_ylim(min_y - offset, max_y + offset)
-    
+    set_ax_bounds(ax, x, y)
+
     # Redraw
     fig.canvas.draw_idle()
     fig.canvas.flush_events()
 
+
 # Initial parameters
 VARIANCE = 0.1
 PERMUTATION_CHANCE = 0.1
-LENGTH = 50
+LENGTH = 500
+WIDTH_VARIATION = 0.0
 INITIAL_ANGLE = 0.0
+INITIAL_WIDTH = 5.0
+START_STEPS = 0
+END_STEPS = 0
 
 # Setup plot
 fig, ax = plt.subplots()
-fig.subplots_adjust(left=0.25, bottom=0.25)
+fig.subplots_adjust(left=0.25, bottom=0.50)
 
 # Initial plot
-x, y = get_crack(VARIANCE, INITIAL_ANGLE, PERMUTATION_CHANCE, LENGTH)
+crack_generator = CrackPathGenerator()
+parameters = CrackParameters(
+    0,
+    INITIAL_WIDTH,
+    LENGTH,
+    INITIAL_ANGLE,
+    VARIANCE,
+    WIDTH_VARIATION,
+    START_STEPS,
+    END_STEPS,
+    PERMUTATION_CHANCE
+)
+x, y = crack_generator(parameters)
 line, = ax.plot(x, y, color='red')
+set_ax_bounds(ax, x, y)
 plt.grid()
-plt.title('Brownian Motion example')
-ax.set_title('Brownian Motion example')
+plt.title('Crack parameter playground')
+ax.set_title('Crack parameter playground')
 
 # Horizontal sliders to control the chance, variance and initial angle.
-ax_chance_slider = fig.add_axes([0.25, 0.05, 0.4, 0.03])
+ax_chance_slider = fig.add_axes([0.25, 0.35, 0.4, 0.03])
 chance_slider = Slider(
     ax=ax_chance_slider,
-    label='Angle chance',
+    label='Chance',
     valmin=0.0,
     valmax=1.0,
     valinit=PERMUTATION_CHANCE,
 )
 
-ax_variance_slider = fig.add_axes([0.25, 0.1, 0.4, 0.03])
+ax_variance_slider = fig.add_axes([0.25, 0.30, 0.4, 0.03])
 variance_slider = Slider(
     ax=ax_variance_slider,
     label='Variance',
@@ -82,17 +96,56 @@ variance_slider = Slider(
     valinit=VARIANCE,
 )
 
-ax_angle_slider = fig.add_axes([0.25, 0.15, 0.4, 0.03])
+ax_angle_slider = fig.add_axes([0.25, 0.25, 0.4, 0.03])
 angle_slider = Slider(
     ax=ax_angle_slider,
     label='Start angle',
     valmin=0.0,
-    valmax=math.pi,
+    valmax=2 * math.pi,
     valinit=INITIAL_ANGLE,
 )
 
+ax_width_variation_slider = fig.add_axes([0.25, 0.20, 0.4, 0.03])
+width_variation_slider = Slider(
+    ax=ax_width_variation_slider,
+    label='Width variation',
+    valmin=0.,
+    valmax=1.,
+    valinit=WIDTH_VARIATION
+)
+
+ax_start_pointiness_slider = fig.add_axes([0.25, 0.15, 0.4, 0.03])
+start_slider = Slider(
+    ax=ax_start_pointiness_slider,
+    label='Start pointiness',
+    valmin=0,
+    valmax=50,
+    valinit=START_STEPS,
+    valstep=1
+)
+
+ax_end_pointiness_slider = fig.add_axes([0.25, 0.10, 0.4, 0.03])
+end_slider = Slider(
+    ax=ax_end_pointiness_slider,
+    label='End pointiness',
+    valmin=0,
+    valmax=50,
+    valinit=END_STEPS,
+    valstep=1
+)
+
+ax_length_slider = fig.add_axes([0.25, 0.05, 0.4, 0.03])
+length_slider = Slider(
+    ax=ax_length_slider,
+    label='Length',
+    valmin=0,
+    valmax=1000,
+    valinit=LENGTH,
+    valstep=1
+)
+
 # Redraw button
-redraw_button_ax = fig.add_axes([0.8, 0.025, 0.1, 0.04])
+redraw_button_ax = fig.add_axes([0.8, 0.20, 0.1, 0.04])
 button = Button(redraw_button_ax, 'Redraw', hovercolor='0.975')
 button.on_clicked(update_plot)
 
