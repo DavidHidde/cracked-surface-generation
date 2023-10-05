95359e81dca323886f207e06fb440b3c1fc8a227
DavidHidde <dahibo@kpnmail.nl>
2023-10-05 14:08:50 +0200

    Add basic setup script

--- src/scenes/generate_dataset.py
+++ src/scenes/generate_dataset.py
@@ -1,11 +1,22 @@
 import os
+
 import bpy
 import argparse
 
-parser = argparse.ArgumentParser(
-    prog='Batch Crack model generator',
-    description='Generate a batch of cracks in the current workspace'
-)
+
+def clear_scene() -> None:
+    """
+    Remove all objects from the scenes
+    """
+    for bpy_data_iter in (
+            bpy.data.collections,
+            bpy.data.objects,
+            bpy.data.meshes,
+            bpy.data.lights,
+            bpy.data.cameras,
+    ):
+        for id_data in bpy_data_iter:
+            bpy_data_iter.remove(id_data, do_unlink=True)
 
 
 def main():
@@ -17,12 +28,16 @@ def main():
         description='Generate a data set consisting of images and labels using models and Blender.'
     )
     parser.add_argument('--models', dest='models_path', type=str, required=True)
-    parser.parse_args()
+    args = parser.parse_args()
 
-    model_files = [file for file in os.listdir(parser.models_path) if file.endswith('.obj')]
-    print(list(model_files))
-    
+    model_files = [file for file in os.listdir(args.models_path) if file.endswith('.obj')]
+
+    clear_scene()
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except Exception as error:
+        print('Something caused the script to crash')
+        print(error)
