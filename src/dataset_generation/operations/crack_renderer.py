c7b8a64e214f24609c1b6acba0bb237b5f891cc8
DavidHidde <dahibo@kpnmail.nl>
2023-10-20 16:44:30 +0200

    Fix file paths

--- src/dataset_generation/operations/crack_renderer.py
+++ src/dataset_generation/operations/crack_renderer.py
@@ -1,3 +1,5 @@
+import os
+
 import bpy
 
 from . import MaterialLoader
@@ -20,14 +22,18 @@ class CrackRenderer:
         Render the current scene with and without the crack marker to a specified
         file name. This name should be without the image extension.
         """
+        output_dir = os.path.dirname(bpy.data.filepath)
+        base_file_path = os.path.join(output_dir, f'{output_name}.png')
+        label_file_path = os.path.join(output_dir, f'{output_name}-label.png')
+
         # First pass: Render without marker
         crack_marker.hide_render = True
-        bpy.context.scene.render.filepath = f'{output_name}.png'
+        bpy.context.scene.render.filepath = os.path.join(output_dir, base_file_path)
         bpy.ops.render.render(write_still=True)
         
         # Second pass: Render with marker
         crack_marker.hide_render = False
-        bpy.context.scene.render.filepath = f'{output_name}-label.png'
+        bpy.context.scene.render.filepath = label_file_path
         
         # Turn off HDRI
         mix_node = bpy.data.worlds['World'].node_tree.nodes['Mix']
@@ -39,4 +45,4 @@ class CrackRenderer:
         mix_node.inputs[0].default_value = 0.
         
         # Threshold the image
-        self.__label_thresholder(f'{output_name}-label.png')
+        self.__label_thresholder(label_file_path)
