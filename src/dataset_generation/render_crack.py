04d89a2908e1c8b84433e1433787e8b1d246ce16
DavidHidde <davidhidde@proton.me>
2025-02-17 08:54:56 +0100

    Fix active pixels not being read correctly

--- src/dataset_generation/render_crack.py
+++ src/dataset_generation/render_crack.py
@@ -24,7 +24,7 @@ def generate_patches(
             start_x, end_x = col_idx * step_size, (col_idx + 1) * step_size
             label_patch = label[start_y:end_y, start_x:end_x]
 
-            if np.sum(label_patch) > parameters.min_active_pixels:
+            if np.sum(label_patch > 0) > parameters.min_active_pixels:
                 img_patch = image[start_y:end_y, start_x:end_x]
                 cv2.imwrite(os.path.join(parameters.image_output_directory, f'crack-{idx + count}.png'), img_patch)
                 cv2.imwrite(os.path.join(parameters.label_output_directory, f'crack-{idx + count}.png'), label_patch)
@@ -41,7 +41,7 @@ def render_crack(parameters: LabelParameters, iteration_index: int) -> int:
     # Check if the label is 'empty'
     img = cv2.imread(rendered_image_path)
     label = cv2.imread(rendered_label_path)[:, :, 0]
-    if np.sum(label) < parameters.min_active_pixels:
+    if np.sum(label > 0) < parameters.min_active_pixels:
         return 0
 
     # All is okay, we split into patches or move and rename the files
